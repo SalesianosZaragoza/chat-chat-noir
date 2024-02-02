6077a2f84df6d704c49e49f267bed95f28003f6e
sylvya0 <lazaro.rosil22@zaragoza.salesianos.edu>
2024-02-02 11:12:06 +0100

    Cambiados  echo cliente y echo servidor

--- .github/chat/client/echo-client.py
+++ .github/chat/client/echo-client.py
@@ -4,7 +4,8 @@ def send_data_to_server(host, port, message):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
-    client_socket.sendall(message.encode())
+    mandar=input("Mensaje a enviar:")
+    client_socket.send(mandar.encode())
 
     data = client_socket.recv(1024)
     print(f"Echoed data: {data.decode()}")
@@ -13,10 +14,16 @@ def send_data_to_server(host, port, message):
 
 if __name__ == "__main__":
     # Set the host and port for the echo server
-    server_host = "127.0.0.1"  # Change this to the IP address of your server
-    server_port = 12345
+    server_host = "10.10.10.223"  # Change this to the IP address of your server
+    server_port = 65433
 
     # Message to be echoed
-    message = "Hello, Echo Server!"
+    message = " "
+    
+   
+    while True:
+        send_data_to_server(server_host, server_port, message)
+        if message == "exit":
+            break
 
-    send_data_to_server(server_host, server_port, message)
+
--- .github/chat/server/echo-server.py
+++ .github/chat/server/echo-server.py
@@ -1,6 +1,10 @@
+import sys
 import socket
+import selectors
+import types
 
-HOST = "10.10.0.219"  # Standard loopback interface address (localhost)
+
+HOST = "10.10.10.223"  # Standard loopback interface address (localhost)
 PORT = 65433  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -11,6 +15,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
+            print(data.decode())
+            sendData = input("N :")
+            conn.send(sendData.encode())
             if not data:
                 break
             conn.sendall(data)
