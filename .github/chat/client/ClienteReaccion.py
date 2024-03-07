13f0eef2edc8b43f05037cbf817d8117cb1c14fb
Andrés García <garcia.miand22@zaragoza.salesianos.edu>
2024-03-07 11:03:35 +0100

    Mikel te quiero

--- .github/chat/client/ClienteReaccion.py
+++ .github/chat/client/ClienteReaccion.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import os
 import time
+import threading
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,31 +14,38 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ##print(port)
 
 def send_msg(msg):
-    sock.sendall(msg.encode())
+    sock.send(msg.encode())
 
 def get_msg():
-    msg = sock.recv(20480).decode()
-    return msg
+    while True:
+        msg = sock.recv(20480).decode()
+        if msg == "exit":
+            print("\nClosing connection")
+            sock.close()
+        else:
+            print(msg)
+        
 
 server = "10.10.10.223"
 port = 65433
 
+
 sock.connect((server, port))
 print("Connecting to " + server + " on port " + str(port) + "\n")
-
+dataa = sock.recv(20480).decode()
+msg = input(dataa)
+send_msg(msg)
 while True:
     #Send data
-    msg = input("Escribe: ")
-    print("Sending '" + msg + "'")
+    t1 = threading.Thread(target=get_msg, args=())
+    t1.start()
+    msg = input()
     send_msg(msg)
+
 
     #Response
     #amnt_exp = len(msg)
     #data = sock.recv(2048)
-    data = get_msg()
+   
 
-    if data == "exit":
-        print("\nClosing connection")
-        sock.close()
-    else:
-        print("Received: \n" + data)
+    
--- .github/chat/server/ServerReaccion.py
+++ .github/chat/server/ServerReaccion.py
@@ -3,9 +3,24 @@ import sys
 import os
 import subprocess
 import time
+import threading
+
 
 #Create a TCP/IP Socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clients = []
+names = []
+
+def on_new_client(clientsocket,addr):
+    username = obten_nombre(clientsocket)
+    while True:
+        msg = get_msg(clientsocket)
+        charla = username + ' : '+ msg
+        send_log(charla)
+        #do some checks and if msg == someWeirdSignal: break:
+        print(username, ' : ', msg)
+        #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
+    clientsocket.close()
 
 ##server = input("Enter server IP: ")
 ##print(server)
@@ -13,42 +28,64 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ##port = int(input("Enter port: "))
 ##print(port)
 
-def send_msg(msg):
-    conn.sendall(msg.encode())
+def send_msg(msg, conna):
+    conna.send(msg.encode())
+def send_log(msg):
+    for c in clients:
+        send_msg(msg, c)
+def obten_nombre(conna):
+    dataa = "Introduzca su nombre: "
+    send_msg(dataa, conna)
+    nombre = get_msg(conna)
+    nombre = comp_nombre(nombre, conna)
+    names.append(nombre)
+    return nombre
+def comp_nombre(nom, conna):
+    for name in names:
+        if nom == name:
+            send_msg("Nombre ya seleccionado\n", conna)
+            return obten_nombre(conna)
+    return nom
 
-def get_msg():
-    msg = conn.recv(20480).decode()
+
+def get_msg(conna):
+    msg = conna.recv(20480).decode()
     return msg
 
-server = "10.10.10.223"
+server = "0.0.0.0"
 port = 65433 
-
+all_threads = []
 #Config
 sock.bind((server, port))
 print("Bound to " + server + " on port " + str(port) + "\n")
 
-sock.listen(1)
+sock.listen(5)
 while True:
     print('Waiting for a connection...') # inside the loop, not before it
     conn, caddr = sock.accept()
-    print("Connected!\n")
+    print(f"Connected to!\n {caddr}")
+    clients.append(conn)
 
-    while True:
-        print("Waiting for a command...")
-        data = get_msg()
-        if data == "hola":
-            print("Me ha dicho hola")
-            send_msg("Gracias por saludar")
-        else:
-            send_msg("Es rudo no saludar")
+    t1 = threading.Thread(target=on_new_client, args=(conn,caddr))
+    t1.start()
+
+    all_threads.append(t1)
+    #while True:
+#        print("Waiting for a command...")
+#        data = get_msg()
+#        if data == "hola":
+#            print("Me ha dicho hola")
+#            send_msg("Gracias por saludar " + username)
+#        else:
+#            send_msg(" ")
 
 
         #Exit
-        if data == "exit":
-            print("\nConnection closed")
-            conn.close()
-            break # go back to the outer accept loop to get the next connection
+#        if data == "exit":
+#            print("\nConnection closed")
+#            conn.close()
+#            break # go back to the outer accept loop to get the next connection
 
-        print("Received '" + data + "'")
-        # ... etc. ...
-        print()
+ #       print("Received '" + data + "'")
+  #      # ... etc. ...
+   #     print()
